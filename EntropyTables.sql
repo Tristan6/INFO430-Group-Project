854511cee0c174b441379ccddf73719b3a90e1d5
Tristan Macell <tristanmacelli@gmail.com>
2019-11-22 13:19:57 -0800

    [ADD] table creation script

---
+++ EntropyTables.sql
@@ -0,0 +1,52 @@
+use [Group3-Final]
+
+CREATE TABLE tblRoomType
+(RoomTypeID INTEGER IDENTITY(1,1) primary key,
+RoomTypeName varchar(50) NOT NULL,
+RoomTypeDescr varchar(500) NULL)
+GO
+
+-- This represents a user account
+CREATE TABLE tblUser
+(UserID INTEGER IDENTITY(1,1) primary key,
+FirstName varchar(50) NOT NULL,
+LastName varchar(50) NOT NULL,
+Email varchar(500) NOT NULL,
+PassHash varchar(500) NOT NULL)
+GO
+
+-- This represents a settlement
+CREATE TABLE tblHouse
+(HouseID INT IDENTITY(1,1) primary key NOT NULL,
+HouseName varchar(50) NOT NULL,
+HouseDescr varchar(500) NULL)
+GO
+
+-- This represents an association with a specific settlement since you 
+-- may have multiple people in one settlement, or live between multiple 
+-- settlements
+CREATE TABLE tblHouseGroup
+(HouseGroupID INT IDENTITY(1,1) primary key NOT NULL,
+UserID INTEGER FOREIGN KEY REFERENCES tblUser (UserID) NOT NULL,
+HouseID INTEGER FOREIGN KEY REFERENCES tblHouse (HouseID) NOT NULL)
+GO
+
+-- This represents a settlement
+CREATE TABLE tblQuadrant
+(QuadrantID INT IDENTITY(1,1) primary key NOT NULL,
+QuadrantName varchar(50) NOT NULL,
+RoomID INTEGER FOREIGN KEY REFERENCES tblRooms (RoomID) NOT NULL)
+GO
+
+-- This represents a settlement
+CREATE TABLE tblItem
+(ItemID INT IDENTITY(1,1) primary key NOT NULL,
+ItemName varchar(50) NOT NULL,
+ImgSrc varchar(150) NULL,
+LocationDescription varchar(200) NULL,
+Condition varchar(50) NOT NULL,
+PurchaseDate DATE NULL,
+ExpirationDate DATE NULL,
+QuadrantID INTEGER FOREIGN KEY REFERENCES tblQuadrant (QuadrantID) NOT NULL,
+ItemTypeID INTEGER FOREIGN KEY REFERENCES tblItemType (ItemTypeID) NOT NULL)
+GO
