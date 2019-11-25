ca9d1af2266124d72ffa2bc2efd2ee1345ddaf77
Tristan Macell <tristanmacelli@gmail.com>
2019-11-25 11:57:46 -0800

    [ADD] sql file with stored procs stubbed out

---
+++ EntropyProcedures.sql
@@ -0,0 +1,19 @@
+use [Group3-Final]
+
+CREATE PROCEDURE insert_Floor
+
+CREATE PROCEDURE insert_House
+
+CREATE PROCEDURE insert_HouseGroup
+
+CREATE PROCEDURE insert_Item
+
+CREATE PROCEDURE insert_ItemType
+
+CREATE PROCEDURE insert_Quadrant
+
+CREATE PROCEDURE insert_Rooms
+
+CREATE PROCEDURE insert_RoomType
+
+CREATE PROCEDURE insert_User
